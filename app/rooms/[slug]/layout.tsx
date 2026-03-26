851f0ae469b8434dcd528f8ac2c40c99a390354a
Pariss Viola <parissviola@Parisss-MacBook-Pro.local>
2026-03-26 08:56:55 -0400

    Remove cinematic room transitions

--- app/rooms/[slug]/layout.tsx
+++ app/rooms/[slug]/layout.tsx
@@ -1,10 +1,7 @@
-import { RoomTransitionProvider } from "@/components/RoomTransitionProvider";
-
 export default function RoomsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <RoomTransitionProvider>{children}</RoomTransitionProvider>;
+  return children;
 }
-

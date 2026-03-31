df7d56da1d8b870b5248f7cc998f6d6267193ad8
Pariss Viola <parissviola@Parisss-MacBook-Pro.local>
2026-03-31 16:38:39 -0400

    Update Ar'Ram'Bam partner logo

--- app/artist-affiliations/page.tsx
+++ app/artist-affiliations/page.tsx
@@ -649,7 +649,7 @@ const PARTNERS: Partner[] = [
   { name: "NBA2K", src: "/partners/nba2k.png", href: "https://nba.2k.com/" },
   { name: "Red Lotus Films", src: "/partners/red-lotus-films.png" },
   { name: "Netflix", src: "/partners/netflix-20260330.png", href: "https://www.netflix.com/" },
-  { name: "arrambam", src: "/partners/arrambam-20260330.jpg" },
+  { name: "arrambam", src: "/rooms/arrambam2.png" },
   { name: "Power Staffing", src: "/partners/power-staffing-20260330.png", href: "https://survivornet.ca/wp-content/uploads/2025/07/Power-Staffing-Solutions-Logo.png" },
   { name: "Arrow Group of Companies", src: "/partners/arrow-group-of-companies-20260330.jpg", href: "https://summit.canamedtechalliance.com/wp-content/uploads/2024/10/images.jpg" },
   { name: "Velour Beauty", src: "/partners/velour-beauty-transparent.png", href: "https://www.velourbeauty.com/" },
